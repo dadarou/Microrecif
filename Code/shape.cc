769773cc15a63873e7bf2860c216e8b724844405
Daniel Roulin <dadaroulin@bluewin.ch>
2024-03-14 18:40:12 +0100

    Started work on the shape module

--- Code/shape.cc
+++ Code/shape.cc
@@ -1,1 +1,32 @@
 #include "shape.h"
+#include <cmath>
+#include <cassert>
+using namespace std;
+
+Segment::Segment(S2d base, double angle, double length)
+    : base(base), angle(angle), length(length)
+{
+    assert(angle >= -M_PI && angle <= M_PI);
+    assert(length >= 0);
+}
+
+S2d Segment::extremity()
+{
+    S2d extremity;
+    extremity.x = base.x + length*cos(angle);
+    extremity.y = base.y + length*sin(angle);
+    return extremity;
+}
+
+double Segment::angularGap(Segment s)
+{
+    double gap = angle - s.angle + M_PI;
+    if (gap > M_PI)
+    {
+        gap -= 2*M_PI;
+    } else if (gap < -M_PI)
+    {
+        gap += 2*M_PI;
+    }
+    return gap;
+}
--- Code/shape.h
+++ Code/shape.h
@@ -3,5 +3,22 @@
 
 constexpr double epsil_zero(0.5);
 
+struct S2d
+{
+    double x = 0.;
+    double y = 0.;
+};
+
+class Segment
+{
+private:
+    S2d base;
+    double angle;
+    double length;
+public:
+    Segment(S2d base, double angle, double length);
+    S2d extremity();
+    double angularGap(Segment s);
+};
 
 #endif
