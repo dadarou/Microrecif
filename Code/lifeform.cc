ffc717dd8411b3e1aad68f7bdf244f94cb1f21ce
Joshua <jo.hrlmn@gmail.com>
2024-03-20 16:01:25 +0100

    Correction class

--- Code/lifeform.cc
+++ Code/lifeform.cc
@@ -2,59 +2,34 @@
 #include "shape.h"
 #include <string>
 
-class Lifeform {
-public :
-    Lifeform(double age, double x, double y){
-        age = age;
-        pos_x = x;
-        pos_y = y;
+Lifeform::Lifeform(double age, double x, double y){
+    age = age;
+    pos_x = x;
+    pos_y = y;
+}
+
+Lifeform::int update_age(double age_max){
+    if (age++ == age_max){
+        return 0; //age est incremente de 1 ou pas ?
+    }else{
+        return 1; //tjrs en vie
+        //est ce que c'est juste de retourner un int
     }
-    int update_age(double age_max){
-        if (age++ == age_max){
-            return 0; //age est incremente de 1 ou pas ?
-        }else{
-            return 1; //tjrs en vie
-            //est ce que c'est juste de retourner un int
-        }
-    }
+}
 
-    ///double age_getter(){
-    ///    return age;
-    ///}
-    ///void age_setter(double age){
-    ///    age = age;
-    ///}
-private :
-    double age;
-    double pos_x;
-    double pos_y;
-};
+///double age_getter(){
+///    return age;
+///}
+///void age_setter(double age){
+///    age = age;
+///}
 
-class Algue {
-public :
+Corail::Corail(int statut, int sens_rot, int st_dev){
+    statut = statut;
+    sens_rotation = sens_rot;
+    st_developpement = st_dev;
+}
 
-private :
-    
-};
-
-class Corail{
-public :
-    Corail(int statut, int sens_rot, int st_dev){
-        statut = statut;
-        sens_rotation = sens_rot;
-        st_developpement = st_dev;
-    }
-private :
-    int statut;
-    int sens_rotation;
-    int st_developpement;
-};
-
-class Scavenger{
-public :
-    Scavenger(int etat){
-        etat = etat;
-    }
-private :
-    int etat;
-};
+Scavenger::Scavenger(int etat){
+    etat = etat;
+}
--- Code/lifeform.h
+++ Code/lifeform.h
@@ -1,6 +1,38 @@
 #ifndef LIFEFORM_H
 #define LIFEFORM_H
 
+class Lifeform {
+public :
+    Lifeform(double age, double x, double y);
+    int update_age(double age_max);
 
+private :
+    double age;
+    double pos_x;
+    double pos_y;
+};
+
+class Algue {
+public :
+
+private :
+    
+};
+
+class Corail{
+public :
+    Corail(int statut, int sens_rot, int st_dev);
+private :
+    int statut;
+    int sens_rotation;
+    int st_developpement;
+};
+
+class Scavenger{
+public :
+    Scavenger(int etat);
+private :
+    int etat;
+};
 
 #endif
